e96b3d0bc66dbf7e03e28cae533f46216f6dc180
PureWater <xliu980625@gmail.com>
2023-05-26 02:16:23 +1000

    修改为C++11语法

--- threadPool/TaskQueue.cpp
+++ threadPool/TaskQueue.cpp
@@ -1,32 +1,17 @@
 #include "TaskQueue.h"
 
-// 往任务队列中添加任务
-void TaskQueue::addTask(Task task)
+/**
+ * 从任务队列中取任务
+ */
+TaskQueue::Task TaskQueue::taskTake()
 {
-	m_mutex.lock();			// 加锁 线程同步 按顺序插入任务
-	m_taskQ.push(task);		// 插入任务
-	m_mutex.unlock();		// 解锁
-}
-
-// 重载版本添加
-void TaskQueue::addTask(callback f, void* arg)
-{
-	m_mutex.lock();				// 加锁 线程同步 按顺序插入任务
-	m_taskQ.push(Task(f, arg));	// 插入任务
-	m_mutex.unlock();			// 解锁
-}
-
-// 获取队列中的任务
-Task TaskQueue::takeTask()
-{
-	m_mutex.lock();				// 加锁
-	Task t;						// 创建一个临时任务保存需要取出的任务
-	// 判断队列中是否有任务
-	if (!m_taskQ.empty())
+	Task task;
+	lock_guard<mutex> lockGuard(taskQueueMutex);
+	if (!taskQueue.empty())
 	{
-		t = m_taskQ.front();	// 保存需要取出的任务
-		m_taskQ.pop();			// 弹出
+		task = move(taskQueue.front());
+		taskQueue.pop();	// 将任务从队列中删除
+		return task;
 	}
-	m_mutex.unlock();			// 解锁
-	return t;					// 返回拿到的任务
+	return nullptr;
 }
--- threadPool/TaskQueue.h
+++ threadPool/TaskQueue.h
@@ -1,45 +1,54 @@
 #pragma once
+/*
+ @Author: Marvin
+ @Date: 2023/05/26 1:06:00
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#ifndef _TASKQUEUE_H_
+#define _TASKQUEUE_H_
+
 #include <queue>
+#include <functional>
 #include <mutex>
-
-using callback = void(*)(void*);
-/**
- * 任务结构体
- */
-struct Task
-{
-	Task()
-	{
-		function = nullptr;
-		arg = nullptr;
-	}
-	Task(callback f, void* arg)
-	{
-		this->arg = arg;
-		this->function = function;
-	}
-
-	callback function;
-	void* arg;
-};
-/**
- * 任务队列类
- */
+#include <future>
+#include <iostream>
+using namespace std;
 class TaskQueue
 {
 public:
-	// 添加任务
-	void addTask(Task task);
-	void addTask(callback f, void* arg);
-	// 取出任务
-	Task takeTask();
-	// 获取队列任务个数
-	inline int taskCount()
-	{
-		return m_taskQ.size();
-	}
+	using Task = function<void()>; // 任务类
+	template<typename F, typename ...Args>
+	auto addTask(F& f, Args &&...args)->future<decltype(f(args...))>;
+	Task taskTake();				// 取任务
+	bool empty() { return taskQueue.empty(); }
 private:
-	std :: queue<Task> m_taskQ;
-	std :: mutex m_mutex;
+	mutex taskQueueMutex;		// 任务队列互斥锁
+	queue<Task> taskQueue;		// 任务队列
 };
 
+template <typename F, typename ...Args>
+auto TaskQueue::addTask(F& f, Args &&...args)->future<decltype(f(args...))> {
+	using RetType = decltype(f(args...));		// 获取函数返回值类型
+	// 将函数封装为无形参的类型 std::bind(f, std::forward<Args>(args)...)：将参数与函数名绑定
+	// packaged_task<RetType()>(std::bind(f, std::forward<Args>(args)...)); 将绑定参数后的函数封装为只有返回值没有形参的任务对象，\
+	这样就能使用get_future得到future对象，然后future对象可以通过get方法获取返回值了
+	// std::make_shared<std::packaged_task<RetType()>>(std::bind(f, std::forward<Args>(args)...)); 生成智能指针，离开作用域自动析构
+	auto task = make_shared<packaged_task<RetType()>>(bind(f, forward<Args>(args)...));
+	lock_guard<mutex> lockGuard(taskQueueMutex); // 插入时上锁，防止多个线程同时插入
+	// 将函数封装为无返回无形参类型，通过lamdba表达式，调用封装后的函数，注意，此时返回一个无形参无返回值的函数对象
+	taskQueue.emplace([task] {(*task)(); });	// emplace直接在队尾进行元素构造并插入
+	return task->get_future();
+}
+
+#endif // !_TASKQUEUE_H_
